d8112d25d961a20d20dab6ccfad77e6cdd94b16c
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-05-25 22:58:10 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -11,13 +11,13 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   return NORMAL;
 }
 
-BreachType classifyTemperatureBreach (BreachType (*Classify(temperatureInC) ) 
+BreachType classifyTemperatureBreach (BreachType (*Classify) 
 
 
 {
  int lowerLimit = 0;
   int upperLimit = 0;
-Return (Classify(temperatureInC));
+Return Classify();
  }
 
 
