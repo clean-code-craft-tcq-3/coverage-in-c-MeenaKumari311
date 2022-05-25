14ec5138a10e356a3e60c2484097a754db6a72e6
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-05-25 22:27:14 +0530

    Update classify.c

--- classify.c
+++ classify.c
@@ -1,19 +1,19 @@
 #include "typewise-alert.h"
-BreachType PASSIVE_COOLING_BREACH(temperatureInC)
+BreachType PASSIVE_COOLING_BREACH_type(temperatureInC)
 {
 lowerLimit = 0;
 upperLimit = 35;
 return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
-BreachType HI_ACTIVE_COOLING(temperatureInC)
+BreachType HI_ACTIVE_COOLING_type(temperatureInC)
 {
 lowerLimit = 0;
 upperLimit = 35;
 return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
-BreachType MED_ACTIVE_COOLING(temperatureInC)
+BreachType MED_ACTIVE_COOLING_type(temperatureInC)
 {
 lowerLimit = 0;
 upperLimit = 35;
