a4462e56ed626c5ee7ec90703e8bd3fadcaeb67e
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-05-26 16:21:38 +0530

    Update and rename classify.c to DefineLimits.c

---
+++ DefineLimits.c
@@ -0,0 +1,18 @@
+#include "typewise-alert.h"
+void DefineLimitsForPASSIVE_COOLING()
+{
+lowerLimit = 0;
+upperLimit = 35;
+}
+
+void DefineLimitsForHI_ACTIVE_COOLING()
+{
+lowerLimit = 0;
+upperLimit = 45;
+}
+
+void DefineLimitsForMED_ACTIVE_COOLING()
+{
+lowerLimit = 0;
+upperLimit = 40;
+}
